7f0c00a5c60a15ba1cb09cf303c927f109e35908
Victoria Cana <victoria.c.cana@gmail.com>
2020-07-19 17:09:41 -0700

    changes

---
+++ Assets/Scripts/EggDestruction.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EggDestruction : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    // void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Bird>() != null)
+        {
+            gameObject.SetActive(false);
+
+            // can also destroy gameobject with Destroy(gameObject);
+        }
+    }
+}
